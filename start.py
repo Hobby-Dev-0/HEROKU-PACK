69bf7b61d3fb574d15d3474f1c88f489aadc4e35
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-08 19:15:52 +0530

    start

--- start.py
+++ start.py
@@ -1,6 +1,2 @@
-from os import system # Dynamic-Userbot
-system ("git clone https://github.com/TeamDynamic/Dynamic-Userbot /root/Dynamic-Userbot && cd Dynamic-Userbot && python3 -m userbot")
-# try af
-print("OH SYSTEM IS CRASHED!!")
-system("cd Dynamic-Userbot && python3 -m userbot")
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @DYNAMIC_USERBOT_SUPPORT")
+from os import system # Andencento
+system ("git clone https://github.com/Team-Andencento/Andencento && cd Andencento && python3 -m userbot")
